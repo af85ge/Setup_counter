7d200eb923a9230e94d8b9df6603022c93391c01
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-22 12:25:43 +0500

    updaitng file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import math
-from numpy.lib.type_check import imag
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 # Euclaidean distance 
 def euclaideanDistance(point, point1):
@@ -11,7 +12,7 @@ def euclaideanDistance(point, point1):
     # print(x, x1)
     distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
     return distance
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("walking1.mp4")
 with mp_pose.Pose(
     min_detection_confidence=0.4,
     min_tracking_confidence=0.4) as pose:
@@ -24,10 +25,19 @@ with mp_pose.Pose(
 
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
+    # image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     height, width = image.shape[:2]
+    
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
     results = pose.process(image)
+    mp_drawing.draw_landmarks(
+    image,
+    results.pose_landmarks,
+    mp_pose.POSE_CONNECTIONS,
+    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
     if results.pose_landmarks:
       coords =np.array([np.multiply([p.x, p.y], [width, height]).astype(int) for p in results.pose_landmarks.landmark])
       # [print(c) for c in coords]
@@ -40,7 +50,7 @@ with mp_pose.Pose(
       distance_bt_feets =euclaideanDistance(coords[31], coords[32])
     
       cv2.putText(image, f"Dsit: {round(distance_bt_feets,3)}", (30,40), cv2.FONT_HERSHEY_PLAIN, 1.6, (0,255,0), 2,cv2.LINE_AA)
-      print(distance_bt_feets)
+      # print(distance_bt_feets)
       if distance_bt_feets<70:
         cv2.putText(image, f"standing", (30,80), cv2.FONT_HERSHEY_PLAIN, 1.4, (0,255,255), 2,cv2.LINE_AA)
       elif distance_bt_feets>100:
@@ -51,7 +61,7 @@ with mp_pose.Pose(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
+    cv2.imshow('MediaPipe Pose', image)
+    if cv2.waitKey(0) & 0xFF == 27:
       break
 cap.release()
